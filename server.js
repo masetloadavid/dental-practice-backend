6ca712731a07811e6435bb49e3b4c2c9a3aac911
masetloadavid <masetloadavid@gmail.com>
2026-06-25 16:59:19 +0200

    Add requireTLS for Gmail SMTP

--- server.js
+++ server.js
@@ -67,6 +67,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
