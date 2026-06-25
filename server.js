ac254e2d9574a29e4176a4db7d1f92893ccb8466
masetloadavid <masetloadavid@gmail.com>
2026-06-25 17:09:36 +0200

    Enable secure SMTP connection in server.js

--- server.js
+++ server.js
@@ -66,8 +66,7 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false,
-  requireTLS: true,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
