12977466ca242892d1d6f9b48a8ab01d96300694
masetloadavid <masetloadavid@gmail.com>
2026-06-26 13:31:28 +0200

    Change SMTP port to 465 and set secure to true
    Updated SMTP configuration to use secure connection.

--- server.js
+++ server.js
@@ -65,8 +65,8 @@ app.use(cors({
 app.use(express.json());
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
