82a92f55e1cf1b3e625f942d862892b97a202b8e
masetloadavid <masetloadavid@gmail.com>
2026-06-26 13:03:04 +0200

    Change SMTP transport security setting to false

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
